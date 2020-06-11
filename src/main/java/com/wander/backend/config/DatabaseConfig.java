12c7328452a7c751a531e30397f71094b470e2ee
Rohit Roy <rohit.roy@esecuresolutions.com>
2020-06-11 12:11:29 +0530

    db details

---
+++ src/main/java/com/wander/backend/config/DatabaseConfig.java
@@ -0,0 +1,31 @@
+package com.wander.backend.config;
+
+/**
+ * @author Rohit Roy
+ */
+import com.zaxxer.hikari.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        config.setUsername(username);
+        config.setPassword(password);
+        return new HikariDataSource(config);
+    }
+}
